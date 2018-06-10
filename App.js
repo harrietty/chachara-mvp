926cddce04b5ebe14351b793e21e8be64462ff3d
Harriet Ryder <harriethryder@gmail.com>
2018-06-10 14:47:37 +0100

    Prevent app loading without fonts

--- App.js
+++ App.js
@@ -4,27 +4,35 @@ import { Provider, connect } from 'react-redux';
 import { Font } from 'expo';
 
 import Nav from './src/Nav';
-import AuthNav from './src/AuthNav';
+import AuthNav from './src/auth/AuthNav';
 
 const store = createStore((state, action) => {});
 
 export default class App extends React.Component {
   state = {
-    signedIn: true
+    signedIn: true,
+    fontLoaded: false
   }
 
-  componentDidMount() {
-    Font.loadAsync({
+  async componentDidMount() {
+    await Font.loadAsync({
       'feather': require('./assets/Fonts/FontAwesome.ttf'),
+    });
+    this.setState({
+      fontLoaded: true
     });
   }
 
   render() {
-    const { signedIn } = this.state;
-    return (
-      <Provider store={store}>
-        {signedIn ? <Nav /> : <AuthNav />}
-      </Provider>
-    );
+    const { signedIn, fontLoaded } = this.state;
+    if (fontLoaded) {
+      return (
+        <Provider store={store}>
+          {signedIn ? <Nav /> : <AuthNav />}
+        </Provider>
+      );
+    } else {
+      return null;
+    }
   }
 }
